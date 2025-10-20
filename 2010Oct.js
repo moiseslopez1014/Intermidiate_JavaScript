8d1bccd560a13cb714a34666e3d4385d2c88fe00
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-20 20:11:30 +0200

    1910ejercicio1Hecho

---
+++ 2010Oct.js
@@ -0,0 +1,2 @@
+//Introduccion a JavaScript Intermedio
+console.log('Introduccion a JavaScript Intermedio');
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+const anchorElement = document.querySelector("#app");
+
+// Exercise 1 Acceso al DOM
+console.log('// Exercise 1 Acceso al DOM');
+
+const pConIdUnico = document.getElementById('idUnicoParrafo');
+console.log(pConIdUnico);
+
+const ex1MismaClase = document.querySelectorAll('.ex1Clase');
+console.log(ex1MismaClase);
+
+//Exercise 2 Cambiar el contenido de un elemento del DOM
+console.log('//Exercise 2 Cambiar el contenido de un elemento del DOM');
+
